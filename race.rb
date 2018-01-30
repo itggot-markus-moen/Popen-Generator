05cf2988376b99dfcb29555cb8d9f004f04a0801
Emanuel-Strom <Emanuel.strom@iggot.se>
2018-01-30 17:44:44 +0100

    Script to generate race. The global variable $racetype tells if it is a sub race or a standard race.

---
+++ race.rb
@@ -0,0 +1,20 @@
+def races()
+    racelist = ["Yuan-Ti", "Orc", "Kobold", "Hobgoblin", "Goblin", "Bugbear", "Triton", "Tiefling", "Tabaxi", "Lizardfolk", "Kenku", "Human", "Half-Orc", "Half-Elf", "Goliath", "Firbolg", "A MOTHA FUCKING DRAGON!!!",
+    ["Black Dragonborn", "Blue Dragonborn", "Brass Dragonborn", "Bronze Dragonborn", "Copper Dragonborn", "Gold Dragonborn", "Green Dragonborn", "Red Dragonborn", "Silver Dragonborn", "White Dragonborn"], 
+    ["Hill Dwarf", "Mountaun Dwarf", "Duergar"], ["High Elf", "Wood Elf", "Drow"], ["Forest Gnome", "Rock Gnome", "Svirfneblin"], ["Lightfoot Halfling", "Stout Halfling"]]
+
+    racenum = rand(racelist.length)
+
+    race = racelist[racenum]
+    $racetype = "standard"
+
+    if  (racelist[racenum])[1].length > 1                    #racenum > 15    sparar denna för säkerhets slull...
+        race = race[rand(race.length)]
+        $racetype = "sub"
+    end
+
+return race
+end
+
+puts races()
+puts $racetype

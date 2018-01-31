abf0f589750d5a6cd445fcd30e16498f3b83eaae
Emanuel-Strom <Emanuel.strom@iggot.se>
2018-01-31 10:47:03 +0100

    A MOTHA FUCKING DRAGON!!!

--- race.rb
+++ race.rb
@@ -1,6 +1,6 @@
 def races()
-    racelist = ["Yuan-Ti", "Orc", "Kobold", "Hobgoblin", "Goblin", "Bugbear", "Triton", "Tiefling", "Tabaxi", "Lizardfolk", "Kenku", "Human", "Half-Orc", "Half-Elf", "Goliath", "Firbolg", "A MOTHA FUCKING DRAGON!!!",
-    ["Black Dragonborn", "Blue Dragonborn", "Brass Dragonborn", "Bronze Dragonborn", "Copper Dragonborn", "Gold Dragonborn", "Green Dragonborn", "Red Dragonborn", "Silver Dragonborn", "White Dragonborn"], 
+    racelist = ["Yuan-Ti", "Orc", "Kobold", "Hobgoblin", "Goblin", "Bugbear", "Triton", "Tiefling", "Tabaxi", "Lizardfolk", "Kenku", "Human", "Half-Orc", "Half-Elf", "Goliath", "Firbolg",
+    ["Black Dragonborn", "Blue Dragonborn", "Brass Dragonborn", "Bronze Dragonborn", "Copper Dragonborn", "Gold Dragonborn", "Green Dragonborn", "Red Dragonborn", "Silver Dragonborn", "White Dragonborn", "A MOTHA FUCKING DRAGON!!!"], 
     ["Hill Dwarf", "Mountaun Dwarf", "Duergar"], ["High Elf", "Wood Elf", "Drow"], ["Forest Gnome", "Rock Gnome", "Svirfneblin"], ["Lightfoot Halfling", "Stout Halfling"]]
 
     racenum = rand(racelist.length)

6ffc7f44f700f616115f9822efc55a70271c01a1
itggot-markus-moen <markus.moen@itggot.se>
2018-02-07 20:44:19 +0100

    Fixed attributes me think.

--- race.rb
+++ race.rb
@@ -1,15 +1,16 @@
 def races()
     racelist = ["Yuan-Ti", "Orc", "Kobold", "Hobgoblin", "Goblin", "Bugbear", "Triton", "Tiefling", "Tabaxi", "Lizardfolk", "Kenku", "Human", "Half-Orc", "Half-Elf", "Goliath", "Firbolg",
     ["Black Dragonborn", "Blue Dragonborn", "Brass Dragonborn", "Bronze Dragonborn", "Copper Dragonborn", "Gold Dragonborn", "Green Dragonborn", "Red Dragonborn", "Silver Dragonborn", "White Dragonborn", "A MOTHA FUCKING DRAGON!!!"], 
-    ["Hill Dwarf", "Mountain Dwarf", "Duergar"], ["High Elf", "Wood Elf", "Drow"], ["Forest Gnome", "Rock Gnome", "Svirfneblin"], ["Lightfoot Halfling", "Stout Halfling"]]
+    ["Hill Dwarf", "Mountain Dwarf", "Duergar"], ["High Elf", "Wood Elf", "Drow"], ["Forest Gnome", "Rock Gnome", "Svirfneblin"], ["Lightfoot Halfling", "Stout Halfling"], ["Protector Aasimar", "Scourge Aasimar", "Fallen Aasimar"]]
 
-    racenum = rand(racelist.length)
+    $racenum = rand(racelist.length)
 
-    race = racelist[racenum]
+    race = racelist[$racenum]
     $racetype = "standard"
 
-    if  (racelist[racenum])[1].length > 1                    
-        race = race[rand(race.length)]
+    if  (racelist[$racenum])[1].length > 1
+        $subracenum = rand(race.length)
+        race = race[$subracenum]
         $racetype = "sub"
     end
 
